5b99b91ca15a903cdead9da2e0a70a1c6632a351
Philip Peterson <1326208+philip-peterson@users.noreply.github.com>
2024-12-01 17:02:52 -0900

    Add text

--- posts/first.go
+++ posts/first.go
@@ -23,13 +23,11 @@ var lookWhereYoureHeaded = model.QuinePost{
 
 			P(Text("An experiment you can try: next time you are on a bicycle, start biking straight. Then, while going straight, look over to your left and focus on an object in the distance. Despite your best efforts, the wheel of the bike will gradually tend toward the left. Why?")),
 
-			P(Text("As humans, we want to know where we're going. But also, due to our psychology, we tend to *go where we are knowing*. If we know about left, we will go left, almost involuntarily. And if you look straight down while lifting a weight, your back and neck will do bad things they were never supposed to do.")),
+			P(Text("As humans, we want to know where we're going. But also, due to our psychology, we tend to "), Em(Text("go where we are knowing")), Text(". If we know about left, we will go left, almost involuntarily. And if you look straight down while lifting a weight, your back and neck will do bad things they were never supposed to do.")),
 
 			P(Text("This insight is relevant to the workplace. If we know where we are going, we will go there, even if unwittingly. But if we don't know, we might look somewhere else and land ourselves in trouble by going somewhere else.")),
 
 			P(Text("And for managers, if your reports cannot be given any time specifically to attaining a goal, just having them know the direction alone, and knowing your team's goals, can get your organization closer to achieving them.")),
-
-			P(Text("Follow this blog for more technology insights in business through [RSS](http://blog.quinefoundation.com/rss.xml), or you can [find us on LinkedIn](https://www.linkedin.com/company/quine-foundation).")),
 		),
 	},
 }
